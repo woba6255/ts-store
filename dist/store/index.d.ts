f1c20b8176ab26074d039dc3347a80c30196c795
woba6255 <53024892+woba6255@users.noreply.github.com>
2021-06-24 10:40:43 +0300

    Delete dist directory

--- dist/store/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './Store';
-export * from './store.types';
-export * from './json.types';
--- dist/store/json.types.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type JsonPrimitives = string | number | boolean | null | undefined | JsonArray | JsonMap;
-interface JsonMap {
-    [key: string]: JsonPrimitives;
-}
-export declare type JsonArray = JsonPrimitives[];
-export declare type Json = JsonArray | JsonMap;
-export {};
